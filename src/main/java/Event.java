ebde84b5f647b34346bc998f3595713bfe859b60
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-09-09 10:06:55 -0700

    Adds getter methods and calculate cost method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,8 +1,5 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Event {
-  private String mNumberOfGuest;
+  private String mNumOfGuest;
   private String mFood;
   private String mDrink;
   private String mVenue;
@@ -11,7 +8,7 @@ class Event {
   private int mHours;
 
   public Event(String guest, String food, String drink, String venue, String music, boolean host, int hours) {
-    mNumberOfGuest = guest;
+    mNumOfGuest = guest;
     mFood = food;
     mDrink = drink;
     mVenue = venue;
@@ -20,8 +17,78 @@ class Event {
     mHours = hours;
   }
 
+  public String getNumOfGuest(){
+    return mNumOfGuest;
+  }
+
+  public String getFood(){
+    return mFood;
+  }
+
+  public String getDrinks(){
+    return mDrink;
+  }
+
+  public String getVenue(){
+    return mVenue;
+  }
+
+  public String getMusic(){
+    return mMusic;
+  }
+
+  public boolean getHost(){
+    return mHost;
+  }
+
+  public int getHours(){
+    return mHours;
+  }
+
   public int calculateCost() {
     int total = 0;
+    if (mNumOfGuest == "0-50"){
+      total += 300;
+    } else if (mNumOfGuest == "51-100"){
+      total += 400;
+    } else if (mNumOfGuest == "101-150"){
+      total += 450;
+    } else if (mNumOfGuest == "151-200"){
+      total += 500;
+    }
+    if (mFood == "No Food"){
+      total += 0;
+    } else if (mFood == "Finger Food"){
+      total += 50;
+    } else if (mFood == "Tapas"){
+      total += 75;
+    }
+    if (mDrink == "No Drinks"){
+      total += 0;
+    } else if (mDrink == "Nonalcoholic"){
+      total += 50;
+    } else if (mDrink == "Full Bar"){
+      total += 200;
+    }
+    if (mVenue == "Bar"){
+      total += (100 * mHours);
+    } else if (mVenue == "Conference Room"){
+      total += (55 * mHours);
+    } else if (mVenue == "Ballroom"){
+      total += (150 * mHours);
+    }
+    if (mMusic == "No Music"){
+      total += 0;
+    } else if (mMusic == "Live Band"){
+      total += (125 * mHours);
+    } else if (mMusic == "DJ"){
+      total += (100 * mHours);
+    }
+    if (mHost == true){
+      total += (60 * mHours);
+    } else {
+      total += 0;
+    }
     return total;
   }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -2,5 +2,60 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class EventTest {
-  
+
+  @Test
+  public void calculateCost_addUpCostOfEvent_1715() {
+    Event testEvent = new Event("0-50", "Tapas", "Full Bar", "Bar", "Live Band", true, 4);
+    int expectedOutput = 1715;
+    assertEquals(expectedOutput, testEvent.calculateCost());
+  }
+
+  @Test
+  public void getNumOfGuest_accuratelyGetsNumOfGuest_String(){
+    Event testEvent = new Event("0-50", "Tapas", "Full Bar", "Bar", "Live Band", true, 4);
+    String expectedOutput = "0-50";
+    assertEquals(expectedOutput, testEvent.getNumOfGuest());
+  }
+
+  @Test
+  public void getFood_accuratelyGetsFood_String(){
+    Event testEvent = new Event("0-50", "Tapas", "Full Bar", "Bar", "Live Band", true, 4);
+    String expectedOutput = "Tapas";
+    assertEquals(expectedOutput, testEvent.getFood());
+  }
+
+  @Test
+  public void getDrinks_accuratelyGetsDrinks_String(){
+    Event testEvent = new Event("0-50", "Tapas", "Full Bar", "Bar", "Live Band", true, 4);
+    String expectedOutput = "Full Bar";
+    assertEquals(expectedOutput, testEvent.getDrinks());
+  }
+
+  @Test
+  public void getVenue_accuratelyGetsVenue_String(){
+    Event testEvent = new Event("0-50", "Tapas", "Full Bar", "Bar", "Live Band", true, 4);
+    String expectedOutput = "Bar";
+    assertEquals(expectedOutput, testEvent.getVenue());
+  }
+
+  @Test
+  public void getMusic_accuratelyGetsMusic_String(){
+    Event testEvent = new Event("0-50", "Tapas", "Full Bar", "Bar", "Live Band", true, 4);
+    String expectedOutput = "Live Band";
+    assertEquals(expectedOutput, testEvent.getMusic());
+  }
+
+  @Test
+  public void getHost_accuratelyGetsHostInfo_true(){
+    Event testEvent = new Event("0-50", "Tapas", "Full Bar", "Bar", "Live Band", true, 4);
+    boolean expectedOutput = true;
+    assertEquals(expectedOutput, testEvent.getHost());
+  }
+
+  @Test
+  public void getHours_accuratelyGetsHours_4(){
+    Event testEvent = new Event("0-50", "Tapas", "Full Bar", "Bar", "Live Band", true, 4);
+    int expectedOutput = 4;
+    assertEquals(expectedOutput, testEvent.getHours());
+  }
 }
